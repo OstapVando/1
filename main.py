0f16d8ba88311406070ab748d6a3d912616f3bdd
OstapVando <ostapvando@gmail.com>
2022-07-13 02:03:47 +0300

    main: collection list was replaced by dictionary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-tree = {}
-depth = 0
+
 
 # Create a class for node of tree
 class Node (object):
@@ -14,26 +13,34 @@ class Node (object):
 
     # search for children in collection method
     def search(self, collection):
+        print('start')
         self.children = []
-        for index in range(len(collection)):
-            item = collection[index]
-            if int(item) == self.index:
+        for index in collection:
+            print(index, collection[index], self.index, (collection[index])==str(self.index))
+            if collection[index] == str(self.index):
                 self.children.append(index)
+
+
 
 
 # Function for creating new child-node
 def createNode(parent, collection):
     newNode = Node(index=None, value=None, parent=parent.index, level=parent.level+1)
     newNode.index = parent.children[-1]
-    newNode.value = collection[4]
+    newNode.value = collection[newNode.index]
+
     return newNode
 
 
 # Recursive function
 def IN_depth (currentNode, collection, depth):
 
+    # search for currentnode's children indexes
     currentNode.search(collection=collection)
-    # print('node: ',{'index ': currentNode.index, 'value ': currentNode.value, 'parent ': currentNode.parent, 'level ': currentNode.level, 'children ': currentNode.children })
+
+    #print node
+    print('node: ',{'index ': currentNode.index, 'value ': currentNode.value, 'parent ': currentNode.parent,
+                    'level ': currentNode.level, 'children ': currentNode.children })
     # there is at least one descedant
     while currentNode.children != []:
         childNode = createNode(currentNode, collection=collection)
@@ -42,6 +49,7 @@ def IN_depth (currentNode, collection, depth):
 
     # base case
     depth = max(depth, currentNode.level)
+    print(collection)
     return depth
 
 
@@ -49,15 +57,18 @@ def IN_depth (currentNode, collection, depth):
 if __name__ == "__main__":
 
     # Data input:
-    n = input()
-    collection = input().split(sep=' ')
-
+    depth = 0
+    n = int(input())
+    inp = input().split(sep=' ')
+    collection = {}
+    for i in range(0, n):
+        collection[i] = inp[i]
+    inp.clear()
     # Create a root-node
     root = Node(value=-1, parent=None, level=1, index=None)
     for item in collection:
-        if item == '-1':
-            root.index = collection.index(str(item))
-
+        if collection[item] == '-1':
+            root.index = item
     # compute
     depth = IN_depth(root, collection, 1)
     print(depth)
